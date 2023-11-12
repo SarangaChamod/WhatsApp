3e57139703d7a2946f3d0fea0bc17c75a65a3dbf
Saranga Chamod <91281208+SarangaChamod@users.noreply.github.com>
2023-11-12 23:19:17 +0530

    upated_message_box

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -1,13 +1,23 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
-import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const InputBox = () => {
   return (
     <View style={styles.container}>
-      {/* <AntDesign name="plus" size={24} color="royalblue" /> */}
-      <TextInput style={styles.input} placeholder="Message" />
-      <MaterialIcons style={styles.send} name="send" size={24} color="white" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Message" />
+        <View style={styles.fileIconContainer}>
+          <MaterialIcons name="attach-file" size={24} color="gray" />
+        </View>
+        <View style={styles.cameraIconContainer}>
+          <MaterialIcons name="camera-alt" size={24} color="gray" />
+        </View>
+      </View>
+
+      <View style={styles.sendIconContainer}>
+        <MaterialIcons name="send" size={24} color="white" />
+      </View>
     </View>
   );
 };
@@ -16,27 +26,39 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     backgroundColor: "transparent",
-    padding: 6,
-    paddingHorizontal:10,
+    padding: 10,
+    paddingHorizontal: 3,
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    borderRadius: 50,
+    flexDirection: "row",
+    flex: 1,
+    paddingHorizontal: 10,
+    marginHorizontal: 10,
   },
   input: {
     flex: 1,
-    backgroundColor: "white",
-    padding:8,
-    paddingHorizontal: 10,
-    marginHorizontal: 10,
-    borderRadius: 50,
-    borderColor:'lightgry',
+    padding: 8,
+    borderColor: "white",
     borderWidth: StyleSheet.hairlineWidth,
-
   },
-  send: {
-    backgroundColor: '#00A884',
+  sendIconContainer: {
+    backgroundColor: "#00A884",
     padding: 10,
     borderRadius: 40,
-    overflow:'hidden'
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fileIconContainer: {
+    marginTop: 8,
+    transform: [{ rotate: "-45deg" }],
+  },
+  cameraIconContainer: {
+    marginTop: 10,
+    paddingHorizontal: 5,
   },
 });
 
 export default InputBox;
-//test
